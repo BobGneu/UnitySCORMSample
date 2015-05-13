3b232c37d6561a6d5a1ab0d2b78538b4c4149710
Bob Chatman <bob.chatman@gmail.com>
2015-05-13 15:51:21 -0700

    Web Export is working properly

---
+++ SCORM Sample/Assets/Scripts/TaskManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Log Manager Initialized.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Complete()
+    {
+        Debug.Log("Quitting!");
+
+        Application.Quit();
+    }
+
+    private void Scorm_Initialize_Complete()
+    {
+        Debug.Log("SCORM Initialized!");
+    }
+
+    private void Scorm_Commit_Complete()
+    {
+        Debug.Log("SCORM Commit Completed!");
+        ScormManager.Terminate();
+    }
+}
